fd03e6e1dee787d8333d25c70061ef48ba8e5304
tuanlam23 <tuanlam16@gmail.com>
2018-10-16 05:16:53 +0000

    template add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get  'static_pages/home'
-  get  'static_pages/help'
-  root 'application#hello'
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
+  root 'static_pages#home'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
